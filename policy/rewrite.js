0ec4ea7295dddb8f64cae7b4191a1df47eaf8c25
Greg Edwards <greg@greglearns.com>
2016-02-12 12:29:02 -0700

    Change {var} to {{var}}.

--- policy/rewrite.js
+++ policy/rewrite.js
@@ -1,7 +1,7 @@
 module.exports=variableRewrites
 
 function variableRewrites(params, str) {
-  var regex = /{([^}]+)}/g
+  var regex = /{{([^}]+)}}/g
   params || (params={})
   return str.replace(regex, (original, name) => {
     return params[name] === undefined ? original : params[name]
