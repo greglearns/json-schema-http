30af9bf1e9fef753ba58c9e718c33ca0dead7f5b
Greg Edwards <greg@greglearns.com>
2016-03-22 13:30:01 -0600

    Add encType checking.

--- schema-stub.js
+++ schema-stub.js
@@ -104,6 +104,12 @@ function stub(schema, server, fns) {
 
     function validateRequest(req, res, next) {
       // console.log('validate Request', req.params, linkSet.schema)
+      if (linkSet.encType && linkSet.encType !== req.contentType()) {
+        res.status(415)
+        res.json({ status: 415, message: "Bad content-type header. Expected: "+linkSet.encType })
+        return
+      }
+
       try {
         var report = isRequestValid(req.params)
         if (report.valid) { next(); return }
